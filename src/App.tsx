4b13ec7a2816cc802349cee27f232a84b6f6bd84
Yeshua <jrleonard3s@gmail.com>
2025-01-24 20:26:10 -0500

    add image list and dynamic column resizing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
+import { ImageList, ImageListItem } from "@mui/material";
 import ImageFrame from "./components/ImageFrame";
+import useWindowDimensions from "./ViewportHooks";
+import "./App.css";
 
 function App() {
+  const { width: windowWidth } = useWindowDimensions();
+  let imageUris = [
+    "http://showcase.leantechniques.com/image/photo-2.jpg",
+    "http://showcase.leantechniques.com/image/photo-10.jpg",
+  ];
+
+  const imageMaxWidth = Math.min(300, windowWidth);
   return (
-    <ImageFrame
-      uri="http://showcase.leantechniques.com/image/photo-10.jpg"
-      title="sandals"
-    />
+    <>
+      <ImageList
+        className="imageList"
+        variant="woven"
+        cols={Math.max(Math.floor(windowWidth / imageMaxWidth), 1)}
+        gap={8}
+      >
+        {imageUris.map((uri) => (
+          <ImageListItem key={uri}>
+            <ImageFrame width={imageMaxWidth} uri={uri}></ImageFrame>
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </>
   );
 }
 
--- src/components/ImageFrame.tsx
+++ src/components/ImageFrame.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-
 interface Props {
   uri: string;
-  title: string;
+  title?: string;
+  width?: number;
 }
 
-const ImageFrame = ({ uri, title }: Props) => {
+const ImageFrame = ({ uri, title, width = 300 }: Props) => {
   return (
     <a href={uri}>
       <img
-        srcSet={`${uri}`}
-        src={`${uri}`}
+        width={width}
+        srcSet={`${uri}?w=161&fit=crop&auto=format&dpr=2 2x`}
+        src={`${uri}?w=161&fit=crop&auto=format`}
         alt={title}
         loading="lazy"
       />
