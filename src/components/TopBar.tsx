1dd4fd03b876a041aba42886a9a9014328c83a77
Yeshua <jrleonard3s@gmail.com>
2025-01-25 01:52:20 -0500

    rename and realign text on app bar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -55,22 +55,13 @@ export default function TopBar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography
             variant="h6"
             noWrap
             component="div"
-            sx={{ flexGrow: 4, display: { xs: "none", sm: "block" } }}
+            sx={{ flexGrow: 3, display: { xs: "none", sm: "block" }, ml: 1 }}
           >
-            MUI
+            Lean Techniques Photo Gallery
           </Typography>
           <Search sx={{ flexGrow: 1, mr: 1 }}>
             <SearchIconWrapper>
