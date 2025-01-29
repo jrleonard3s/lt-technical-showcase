18d33eaef072fe90152678426402d9d7b85feb41
Yeshua <jrleonard3s@gmail.com>
2025-01-28 19:55:22 -0500

    Add tests for ImageFrame

--- test/ImageFrame.test.tsx
+++ test/ImageFrame.test.tsx
@@ -1,8 +1,40 @@
-import { describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
 import "@testing-library/jest-dom/vitest";
+import ImageFrame from "../src/components/ImageFrame";
+import { Photo } from "../src/types";
+import test_image from "../src/assets/test_image.png";
+import { render, screen } from "@testing-library/react";
 
 describe("ImageFrame", () => {
-  it("placeholder", () => {
-    expect(true).toBeTruthy();
+  beforeEach(() => {
+    document.body.innerHTML = "";
+  });
+
+  it("Displays an image", () => {
+    const photo: Photo = {
+      photoId: 1,
+      url: test_image,
+      albumId: 1,
+      title: "test title",
+    };
+
+    render(<ImageFrame photo={photo} />);
+    const image: HTMLImageElement = screen.getByAltText("test title");
+
+    expect(image.src).toContain("test_image");
+  });
+
+  it("Links to the image src", () => {
+    const photo: Photo = {
+      photoId: 1,
+      url: test_image,
+      albumId: 1,
+      title: "test title",
+    };
+
+    render(<ImageFrame photo={photo} />);
+    const link = screen.getByRole("link");
+
+    expect(link).toHaveAttribute("href", test_image);
   });
 });
